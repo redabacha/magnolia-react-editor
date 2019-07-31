48634499e5ae9073bb9d926fbb7f2334e5f25692
Robert Šiška <robert.siska@magnolia-cms.com>
2019-07-31 10:20:57 +0200

    use delivery endpoint

--- webresources-sources/src/environments/environment.ts
+++ webresources-sources/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   mgnEditMode: true,
-  restUrl: 'http://localhost:8080/.rest/angular-endpoint/angular-minimal',
+  restUrl: 'http://localhost:8080/.rest/delivery/pages/v1',
   configRestUrl: 'http://localhost:8080/.rest/headless/v1/mapping/website/angular-minimal',
   damUrl: 'http://localhost:8080/dam/'
 };
