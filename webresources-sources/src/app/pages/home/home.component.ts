d40b5eaed712b7b6dc5730a75a04589965464b8c
Robert Šiška <robert.siska@magnolia-cms.com>
2019-07-31 10:42:34 +0200

    button for show/hide an area

--- webresources-sources/src/app/pages/home/home.component.ts
+++ webresources-sources/src/app/pages/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  secondaryAreaVisible = true;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onClickMe() {
+    console.log('clicked!');
+    this.secondaryAreaVisible = !this.secondaryAreaVisible;
   }
 }
