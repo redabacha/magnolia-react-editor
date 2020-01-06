0ce8b8c35757d7153dc8970f5c3eaeadfefd19b4
Dominic Nguyen <canh.nguyen@magnolia-cms.com>
2020-01-06 14:57:13 +0700

    MGNLFE-7: Removes enableMgnlRenderer.js

--- react-renderer/src/component/Page/Page.js
+++ react-renderer/src/component/Page/Page.js
@@ -2,37 +2,59 @@ import React, { Component } from 'react';
 import { TemplateAnnotations } from '@magnolia/template-annotations';
 import PropTypes from 'prop-types';
 import { Comment } from '../Comment';
-import { RendererContext } from '../../util';
+import { RendererProvider } from '../../util';
 
 class Page extends Component {
     static propTypes = {
-        children: PropTypes.elementType
+        children: PropTypes.elementType,
+        templateDefinitions: PropTypes.object,
+        content: PropTypes.object,
+        componentMappings: PropTypes.object
     }
 
     static defaultProps = {
-        children: null
+        children: null,
+        templateDefinitions: null,
+        content: null,
+        componentMappings: {}
     }
 
-    static contextType = RendererContext;
+    constructor(props) {
+        super(props);
+        const { templateDefinitions, content, componentMappings } = props;
+        const isInPageEditor = Boolean(window.parent && window.parent.mgnlRefresh);
+
+        this.state = {
+            templateDefinitions,
+            // eslint-disable-next-line react/no-unused-state
+            componentMappings,
+            content,
+            inPageEditor: isInPageEditor
+        };
+    }
 
     render() {
-        const { inPageEditor } = this.context;
-        const { content, templateDefinitions } = this.context;
+        const { inPageEditor } = this.state;
+        const { content, templateDefinitions } = this.state;
         const { children } = this.props;
         const openComment = TemplateAnnotations.getPageCommentString(content, templateDefinitions);
         if (inPageEditor) {
             return (
-                <div>
-                    <Comment text={openComment} />
-                    <Comment text="/cms:page" />
-                    {children}
-                </div>
+                <RendererProvider value={this.state}>
+                    <div>
+                        <Comment text={openComment} />
+                        <Comment text="/cms:page" />
+                        {children}
+                    </div>
+                </RendererProvider>
             );
         }
         return (
-            <div>
-                {children}
-            </div>
+            <RendererProvider value={this.state}>
+                <div>
+                    {children}
+                </div>
+            </RendererProvider>
         );
     }
 }
--- react-renderer/src/index.js
+++ react-renderer/src/index.js
@@ -1,2 +1,2 @@
 export { Area, Comment, Page } from './component';
-export { RendererContext, enableMgnlRenderer } from './util';
+export { RendererContext } from './util';
