87944fe7e1cadd42f4e67f6743bffbf8678e71f0
Dominic Nguyen <canh.nguyen@magnolia-cms.com>
2020-02-20 11:40:06 +0700

    MGNLFE-20: Fixes nested component

--- packages/react-renderer/src/util/constants.js
+++ packages/react-renderer/src/util/constants.js
@@ -1,4 +1,5 @@
 export default {
     CLOSED_AREA_COMMENT: '/cms:area',
-    CLOSED_COMPONENT_COMMENT: '/cms:component'
+    CLOSED_COMPONENT_COMMENT: '/cms:component',
+    TEMPLATE_ID_PROP: 'mgnl:template'
 };
