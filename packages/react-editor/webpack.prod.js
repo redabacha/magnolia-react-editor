dc4628151c538c1b8826e7a0034958435790f950
Bartosz Staryga <bartosz.staryga@magnolia-cms.com>
2021-01-19 14:50:29 +0100

    Fix for Super expression must either be null or a functio error

--- packages/react-editor/webpack.prod.js
+++ packages/react-editor/webpack.prod.js
@@ -1,3 +1,4 @@
+const TerserPlugin = require("terser-webpack-plugin");
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -6,5 +7,13 @@ module.exports = merge(common, {
     devtool: 'source-map',
     output: {
         filename: 'mgnl-react-editor.min.js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                keep_fnames: true,
+            },
+        })],
     }
 });
