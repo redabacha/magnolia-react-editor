6411c977d55d1855140c1907ad1a1bce9510e340
Reda Bacha <mohamedredabacha@hollandandbarrett.com>
2021-03-18 15:53:50 +0000

    feat: export hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/EditableArea';
 export * from './components/EditableComponent';
 export * from './components/EditablePage';
+export * from './hooks';
 export * from './util';
